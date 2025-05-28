05ec3e382040fa1347e0505a7de5d19abe5c8fb6
SofiaPV <99967095+SofiaPV@users.noreply.github.com>
2025-05-28 21:36:36 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+    'N_STATES': 5,
+    'N_ACTIONS': 2,
+    'GAMMA': 0.99,
+    'LR': 0.0001,
+    'BATCH_SIZE': 32,
+    'EPSILON_START': 1.0,
+    'EPSILON_MIN': 0.01,
+    'EPSILON_DECAY': 0.995,
+    'TARGET_UPDATE': 200,
+    'MEM_SIZE': 50000,
+}
